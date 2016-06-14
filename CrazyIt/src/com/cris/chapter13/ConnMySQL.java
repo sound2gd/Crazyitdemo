94e8a2cb418e91208104ab76e18265c4628187de
sound2gd <429354772@qq.com>
2016-06-14 21:34:59 +0800

    JDBC readme and demo

---
+++ CrazyIt/src/com/cris/chapter13/ConnMySQL.java
@@ -0,0 +1,45 @@
+package com.cris.chapter13;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.Statement;
+
+/**
+ * 测试JDBC编程步骤连接MySQL
+ * 
+ * @author cris
+ *
+ */
+public class ConnMySQL {
+
+	public static void main(String[] args) throws Exception {
+		// 1，加载驱动
+		Class.forName("com.mysql.jdbc.Driver");
+		// 2，获得数据库连接
+		Connection connection = DriverManager.getConnection(
+				"jdbc:mysql://127.0.0.1:3306/educ", "root", "wang");
+		// 3，获得Statement来执行SQL
+		Statement statement = connection.createStatement();
+		// 4，执行SQL
+		ResultSet result = statement.executeQuery("select * from test_a");
+		// 5，操作结果集
+		ResultSetMetaData metaData = result.getMetaData();
+		int columnCount = metaData.getColumnCount();
+		for (int i = 0; i < columnCount; i++) {
+			System.out.print(metaData.getColumnName(i+1)+"\t");
+		}
+		System.out.print("\n");
+		while (result.next()){
+			for (int i = 0; i < columnCount; i++) {
+				System.out.print(result.getString(i+1)+"\t");
+			}
+//			System.out.print("\n");
+			System.out.println();
+		}
+		// 6，释放数据库资源
+		connection.close();
+	}
+
+}
---
+++ CrazyIt/src/com/cris/chapter13/JDBC.md
@@ -0,0 +1,39 @@
+## 1，JDBC简介
+JDBC4.2包括如下常用接口和实现类
+
+***
+`DriverManager`:管理JDBC驱动的服务类，主要用来获得数据库连接java.sql.Connection
+
+`Connection`:代表数据库的连接对象，每个Connection都代表一个物理连接会话。
+该接口包括如下常用方法:
+- Statement createStatement(),返回一个Statement对象
+- PreparedStatement prepareStatement(String sql)，返回一个预编译的Statement对象
+- CallableStatement prepareCall(String sql):返回一个CallableStatement对象，主要用于调用存储过程
+
+`Statement`对象即可用于执行DDL,DCL语句，也可用于执行DML语句，还可以用于执行SQL查询。
+常用方法是:
+- ResultSet executeQuery(String sql):用于执行查询语句，只能用于执行查询语句
+- int executeUpdate(String sql):用于执行DML语句和DDL语句，DML返回受影响的行数，DDL返回0
+- boolean execute(String sql):可执行任何SQL语句
+
+`ResultSet`对象是结果集对象，包含访问查询结果的方法。
+有close(),absolute(结果集指针移到第几行),beforeFirst()指针定义到首行等等来访问结果集
+
+
+以上就是JDBC编程最主要的对象。
+
+## 2,JDBC编程步骤
+JDBC一般编程步骤包括如下:
+1，加载驱动
+2,获得连接
+3，获取Statament对象
+4，执行sql
+5,得到所要的结果集数据
+6，回收数据库资源
+
+
+
+
+
+
+
